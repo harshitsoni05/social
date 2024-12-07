0f202493e0bf550b6cf8e94c6a637f753059dd75
Harshit Soni <harshitsoni@jklu.edu.in>
2024-12-07 12:23:20 +0530

    Added spring intials for notifications and searchproducts

---
+++ searchproducts/src/main/java/com/social/searchproducts/SearchproductsApplication.java
@@ -0,0 +1,13 @@
+package com.social.searchproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchproductsApplication.class, args);
+	}
+
+}
---
+++ searchproducts/src/test/java/com/social/searchproducts/SearchproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.searchproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
